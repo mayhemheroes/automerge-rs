afe488814d5c588c91c2db4d535928b281a53cc0
Aziz Rizayev <rizaziz2002@forallsecure.com>
2026-04-22 15:47:19 +0000

    Fix build: use upstream rust/automerge/fuzz, drop orphaned automerge/fuzz

--- automerge/fuzz/fuzz_targets/load_incremental.rs
+++
@@ -1,9 +0,0 @@
-#![no_main]
-use libfuzzer_sys::fuzz_target;
-
-fuzz_target!(|data: Vec<&[u8]>| {
-    let mut ac = automerge::AutoCommit::default();
-    for update in data {
-        let _ = ac.load_incremental(update);
-    }
-});
